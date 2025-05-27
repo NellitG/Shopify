b0cc8ac5e425e9d9e69da3045262e30a55a6fd78
NellitG <kamaunelson157@gmail.com>
2025-05-27 18:18:50 +0300

    created API logic for each model

--- shopify/views.py
+++ shopify/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Customer, Cart, Wishlist, Product, Category, Order, OrderItem, Payment, Shipment
+from .serializers import CustomerSerializer, CartSerializer, WishlistSerializer, ProductSerializer, CategorySerializer, OrderSerializer, OrderItemSerializer, PaymentSerializer, ShipmentSerializer
 
 # Create your views here.
+class CustomerListCreateView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CartListCreateView(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class WishlistListCreateView(generics.ListCreateAPIView):
+    queryset = Wishlist.objects.all()
+    serializer_class = WishlistSerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer  
+
+class OrderItemListCreateView(generics.ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+class PaymentListCreateView(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class ShipmentListCreateView(generics.ListCreateAPIView):
+    queryset = Shipment.objects.all()
+    serializer_class = ShipmentSerializer
