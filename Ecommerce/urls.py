2c0ac582df8ed46b8775553c699fecbf88ccfb0b
NellitG <kamaunelson157@gmail.com>
2025-05-28 14:50:28 +0300

    added url paths

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('customers/', views.CustomerListCreateView.as_view(), name='customer-list-create'),
+    path('carts/', views.CartListCreateView.as_view(), name='cart-list-create'),
+    path('wishlists/', views.WishlistListCreateView.as_view(), name='wishlist-list-create'),    
+    path('products/', views.ProductListCreateView.as_view(), name='product-list-create'),
+    path('categories/', views.CategoryListCreateView.as_view(), name='category-list-create'),
+    path('orders/', views.OrderListCreateView.as_view(), name='order-list-create'),
+    path('order-items/', views.OrderItemListCreateView.as_view(), name='order-item-list-create'),
+    path('payments/', views.PaymentListCreateView.as_view(), name='payment-list-create'),
+    path('shipments/', views.ShipmentListCreateView.as_view(), name='shipment-list-create'),
 ]
