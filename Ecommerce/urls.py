37c61fe88453ecca04320a289182483a3c0e2618
NellitG <kamaunelson157@gmail.com>
2025-05-28 17:03:14 +0300

    created paths for each feature

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from shopify import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +30,5 @@ urlpatterns = [
     path('order-items/', views.OrderItemListCreateView.as_view(), name='order-item-list-create'),
     path('payments/', views.PaymentListCreateView.as_view(), name='payment-list-create'),
     path('shipments/', views.ShipmentListCreateView.as_view(), name='shipment-list-create'),
+    
 ]
