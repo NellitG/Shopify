a600a89eb03e8f7349bb42294850a9f10d822a2d
NellitG <kamaunelson157@gmail.com>
2025-05-30 17:54:37 +0300

    registered features in admin.py

--- shopify/admin.py
+++ shopify/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+    Customer, Category, Product, Cart, CartItem,
+    Wishlist, Order, OrderItem, Payment, Shipment
+) 
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Wishlist)   
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Payment)
+admin.site.register(Shipment)
