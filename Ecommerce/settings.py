fb81fb077df7d111f930889193f4cb347da14e83
NellitG <kamaunelson157@gmail.com>
2025-06-23 11:52:33 +0300

    change database from dbsqlite3 to Postgres|Railway

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -77,8 +77,17 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'LXIAoOAipnTmHNNtjmsZFTkJRrUcfAXa',
+
+        'HOST': 'crossover.proxy.rlwy.net',
+
+        'PORT': '19007',
+
     }
 }
 
